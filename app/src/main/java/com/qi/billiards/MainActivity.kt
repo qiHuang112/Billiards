e94d18bb78915fe9ad36dc7de18079bf3e564157
huangqi1 <huangqi1@eastmoney.com>
2023-04-11 14:03:18 +0800

    init project.

---
+++ app/src/main/java/com/qi/billiards/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.qi.billiards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.qi.billiards.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}

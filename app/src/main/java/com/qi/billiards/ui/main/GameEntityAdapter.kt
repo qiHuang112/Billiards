e41afa4d66c9ca5820418c448b58ed40877d8239
qiHuang112 <526375442@qq.com>
2023-11-09 13:43:17 +0800

    历史记录加上数据概览

--- app/src/main/java/com/qi/billiards/ui/main/GameEntityAdapter.kt
+++ app/src/main/java/com/qi/billiards/ui/main/GameEntityAdapter.kt
@@ -2,9 +2,11 @@ package com.qi.billiards.ui.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.google.gson.Gson
 import com.qi.billiards.config.Config
 import com.qi.billiards.databinding.ItemGameBinding
 import com.qi.billiards.db.GameEntity
+import com.qi.billiards.game.ZhuiFenGame
 import com.qi.billiards.ui.base.BaseBindingAdapter
 import com.qi.billiards.util.format
 import java.util.Date
@@ -20,11 +22,18 @@ class GameEntityAdapter(
 
     override fun onBindViewHolder(holder: BaseBindingViewHolder<ItemGameBinding>, position: Int) {
         val game = games[position]
+        val gameDetail = Gson().fromJson(game.game.detail, ZhuiFenGame::class.java)
+        val players = gameDetail.players.sortedBy { it.name }
+        val p1 = players.getOrNull(0)
+        val p2 = players.getOrNull(1)
+        val p3 = players.getOrNull(2)
         holder.binding.apply {
-            tvId.text = game.game.gameId.toString()
+            tvBase.text = "x${gameDetail.base}"
             tvGameType.text = Config.gameType[game.game.gameType] ?: "未知"
             tvStartTime.text = game.game.startTime?.let { Date(it) }.format("yyyy年MM月dd日HH:mm:ss")
-
+            tvP1.text = p1?.let { "${it.name}(${it.score})" }
+            tvP2.text = p2?.let { "${it.name}(${it.score})" }
+            tvP3.text = p3?.let { "${it.name}(${it.score})" }
             root.setOnClickListener {
                 onClickGame(position)
             }
